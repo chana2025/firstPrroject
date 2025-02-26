2b9f5b649f0b3b7d479e038b964bfb9065a35c13
chana2025 <chanif6786@gmail.com>
2025-02-26 09:29:06 +0200

    add to ignore list without json file

---
+++ Lesson1Chani/DelegateLesson/DelegateLesson/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLesson
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public  Address myAddress{ get; set; }
+        public Student()
+        {
+            myAddress=new Address();
+            myAddress.onChangeAddress = func2;
+        }
+
+        public void func2() {
+
+            Console.WriteLine("wellcome ....");
+        }
+    }
+}
